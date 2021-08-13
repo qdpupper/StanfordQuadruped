4203b3d446b671daa48c3fad6f8c43ed1bc93c5b
JueDongli <forecy@126.com>
2021-08-13 09:17:56 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,38 +3,6 @@ yes | pip3 install numpy transforms3d pigpio pyserial
 yes | pip install numpy transforms3d pigpio pyserial
 yes | sudo pip install numpy transforms3d pigpio pyserial
 
-cd ..
-
-# UDPComms
-git clone https://github.com/qdpupper/UDPComms.git
-cd UDPComms
-sudo bash install.sh
-cd ..
-
-# PS4Joystick
-#git clone https://github.com/stanfordroboticsclub/PS4Joystick.git
-git clone https://github.com/qdpupper/PS4Joystick.git
-cd PS4Joystick
-sudo bash install.sh
-cd ..
-
-# joystick.service
-#git clone https://github.com/stanfordroboticsclub/PupperCommand.git
-git clone https://github.com/qdpupper/PupperCommand.git
-cd PupperCommand
-sudo bash install.sh
-cd ..
-
-#sudo systemctl enable joystick
-
-#wget https://github.com/joan2937/pigpio/archive/v74.zip
-#unzip v74.zip
-#cd pigpio-74
-#make
-#sudo make install
-#cd ..
-
-cd StanfordQuadruped
 sudo ln -s $(realpath .)/robot.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable robot
